09e1229d1b28b3e8d51b95452dd64b19e0bbd0bf
sonofmath <sonofmath3.14@hotmail.com>
2018-09-11 12:26:20 -0600

    st upload of kattis problem

---
+++ Kattis1/src/main/java/com/github/sonofmath/kattis/kattis1/Soylent.java
@@ -0,0 +1,55 @@
+
+package com.github.sonofmath.kattis.kattis1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author jrmathson
+ */
+public class Soylent {
+    public static void main(String [] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int[] drinks;
+        while(in.hasNextLine()) {
+            Soylent soylent = new Soylent();
+            soylent.readTestCases(in.nextInt());
+            soylent.solve();
+            soylent.write();
+        }
+    }
+    
+    Scanner in = new Scanner(System.in);
+    
+    int cases;
+    int[] drinks;
+    
+    public void readTestCases(int _cases) {
+        if (_cases < 0) {
+            cases = _cases*(-1);
+        } else {
+            cases = _cases;
+        }
+        drinks = new int[cases];
+        for (int i = 0; i < drinks.length; i++) {
+            drinks[i] = in.nextInt();
+        }
+    }
+    
+    private boolean haveTestCase() {
+        return in.hasNextInt();
+    }
+    
+    void solve() {
+        for (int i = 0; i < drinks.length; i++) {
+            drinks[i] = drinks[i]/400;
+        } 
+    }
+    
+    void write() {
+        for (int i = 0; i < drinks.length; i++) {
+            System.out.println(drinks[i]);
+        }
+    }    
+}
+    
+
---
+++ Kattis1/src/test/java/com/github/sonofmath/kattis/kattis1/SoylentTest.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.sonofmath.kattis.kattis1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jrmathson
+ */
+public class SoylentTest {
+    
+    public SoylentTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Soylent.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        Soylent.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of readTestCases method, of class Soylent.
+     */
+    @Test
+    public void testReadTestCases() {
+        System.out.println("readTestCases");
+        int _cases = 0;
+        Soylent instance = new Soylent();
+        instance.readTestCases(_cases);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of solve method, of class Soylent.
+     */
+    @Test
+    public void testSolve() {
+        System.out.println("solve");
+        Soylent instance = new Soylent();
+        instance.solve();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of write method, of class Soylent.
+     */
+    @Test
+    public void testWrite() {
+        System.out.println("write");
+        Soylent instance = new Soylent();
+        instance.write();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /**
+     * Test of solve method, of class Soylent.
+     */
+    
+}
