83bf4ed4e844c1fec054facfb325e6a97189ebff
sonofmath <sonofmath3.14@hotmail.com>
2018-09-26 21:31:39 -0600

    Initial push after a small set up

---
+++ collection_hw/src/main/java/com/github/sonofmath/collection_hw/Item.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.sonofmath.collection_hw;
+
+/**
+ *
+ * @author jrmathson
+ */
+class Item {
+    int valueInDollars;
+    String name;
+    Item(String _name, int _valueInDollars) {
+        name = _name;
+        valueInDollars = _valueInDollars;
+    }
+}
