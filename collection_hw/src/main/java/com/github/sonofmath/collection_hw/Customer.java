d738d5395b86941ccd33b55d76a2b26f0c2f55eb
sonofmath <sonofmath3.14@hotmail.com>
2018-09-27 09:30:21 -0600

    Added Customer

---
+++ collection_hw/src/main/java/com/github/sonofmath/collection_hw/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.sonofmath.collection_hw;
+
+/**
+ *
+ * @author jrmathson
+ */
+class Customer {
+    int acctNumber;
+    String name;
+    int valueInDollars;
+    
+    Customer(String _name, int _valueInDollars) {
+        name = _name;
+        valueInDollars = _valueInDollars;
+    }
+    
+}
