1bb625646e5ed90c86a772fc68f1448d1fcaad5b
sonofmath <sonofmath3.14@hotmail.com>
2018-10-02 21:03:19 -0600

    initial commit. Nothing works yet

---
+++ threads/src/main/java/threads/ListSorter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+/**
+ *
+ * @author sonof
+ */
+public class ListSorter {
+
+    public ListSorter() {
+    }
+    
+}
---
+++ threads/src/main/java/threads/ListSpeedTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+/**
+ *
+ * @author jemathson
+ */
+public class ListSpeedTest {
+
+    void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void waitTilFinish() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setAction(ListSorter listSorter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
