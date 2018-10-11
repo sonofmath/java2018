0326a6b3b6803836606c8e8adfee1bc7d98d3919
sonofmath <sonofmath3.14@hotmail.com>
2018-10-10 21:10:58 -0600

    Initial setup

---
+++ thread_hw/src/main/java/sonofmath/thread_hw/OurJavaClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sonofmath.thread_hw;
+
+/**
+ *
+ * @author jrmathson
+ */
+public class OurJavaClass {
+    Instructor drmacevoy = new Instructor();
+    Students student = new Students();
+}
